49b870bc699ad175f4714d88c65fcdf450f6433c
win10 <komalsubhra6768@gmail.com>
2020-04-15 19:01:25 -0400

    (Komal Subhra) don with java code of splash screen

--- app/src/main/java/com/example/c0772144_w2020_mad3125_fp/SplashScreenActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_fp/SplashScreenActivity.java
@@ -1,13 +1,29 @@
 package com.example.c0772144_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
+ Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
     }
 }
