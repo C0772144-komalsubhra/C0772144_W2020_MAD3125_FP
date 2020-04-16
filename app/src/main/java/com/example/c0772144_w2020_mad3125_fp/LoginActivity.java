cfbb8cb973588b0cfaff19bfb40ac63a7e05f32f
win10 <komalsubhra6768@gmail.com>
2020-04-15 20:36:21 -0400

    (Komal Subhra) Created Login Screen Ui design

--- app/src/main/java/com/example/c0772144_w2020_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_fp/LoginActivity.java
@@ -1,11 +1,23 @@
 package com.example.c0772144_w2020_mad3125_fp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
+    private TextInputEditText edtEmailIdText;
+    private TextInputEditText edtPasswordText;
+    private TextInputLayout edtEmailId;
+    private TextInputLayout edtPassword;
+    private Button btnlogin;
+    private Switch swchRememberMe;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
