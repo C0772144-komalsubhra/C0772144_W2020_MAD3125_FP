7013714a83439a01184a81f73d179ad5ee535799
win10 <komalsubhra6768@gmail.com>
2020-04-18 18:41:12 -0400

    (Komal Subhra)  validation on email and passsword in login

--- app/src/main/java/com/example/c0772144_w2020_mad3125_fp/CustomerListActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_fp/CustomerListActivity.java
@@ -1,14 +1,90 @@
 package com.example.c0772144_w2020_mad3125_fp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class CustomerListActivity extends AppCompatActivity {
+    private RecyclerView rvCustomerList;
+    private ArrayList customerArrayList;
+    private ArrayList tempCustomerArrayList;
+    private ArrayList<Customer> tempCustomerListArrayList;
+    private CustomerAdapter customerAdapter;
+    private ImageView barAddCustomer;
+    private ImageView barLogout;
+
+    @BindView(R.id.imgBtnAddCustomer)
+    ImageButton ImgBtnAddCustomer;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_list);
+        ButterKnife.bind(this);
+
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        View view = getSupportActionBar().getCustomView();
+
+        barAddCustomer = view.findViewById(R.id.barAddCustomer);
+        barLogout = view.findViewById(R.id.barLogout);
+
+        rvCustomerList = findViewById(R.id.rvCustomerList);
+
+        barAddCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(CustomerListActivity.this, AddCustomerActivity.class);
+                startActivity(mIntent);
+            }
+        });
+
+        barLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(CustomerListActivity.this, LoginActivity.class);
+                startActivity(mIntent);
+            }
+        });
+
+        loadCustomers();
+        //readJson();
+        //customerAdapter = new CustomerAdapter(DataRepository.getInstance().getCustomerDataList());
+        customerAdapter = new CustomerAdapter(customerArrayList);
+        RecyclerView.LayoutManager mLinearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        rvCustomerList.setLayoutManager(mLinearLayoutManager);
+        rvCustomerList.setAdapter(customerAdapter);
+
+        ImgBtnAddCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(CustomerListActivity.this, AddCustomerActivity.class);
+                startActivity(mIntent);
+            }
+        });
+    }
+
+    private void loadCustomers()
+    {
+        DataProvider.getInstance().loadData();
+        customerArrayList = new ArrayList();
+        HashMap<String, Customer> customerHashMap = DataRepository.getInstance().getCustomerMap();
+        Collection<Customer> demoValues = customerHashMap.values();
+        tempCustomerArrayList = new ArrayList<>(demoValues);
+        customerArrayList.addAll(tempCustomerArrayList);
     }
 }
