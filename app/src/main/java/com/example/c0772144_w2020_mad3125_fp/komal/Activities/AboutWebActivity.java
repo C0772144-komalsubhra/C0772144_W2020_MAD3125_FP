da84b7c58dd96e72060070f5a12037f9b84b556e
win10 <komalsubhra6768@gmail.com>
2020-04-20 15:13:17 -0400

    done with about web activty

--- app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AboutWebActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AboutWebActivity.java
@@ -1,24 +1,26 @@
 package com.example.c0772144_w2020_mad3125_fp.komal.Activities;
 
-class AboutWebActivity  : AppCompatActivity()  {
-        override fun onCreate(savedInstanceState: Bundle?){
+class AboutWebActivity : AppCompatActivity()  {
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
-        val myWebView:WebView=findViewById(R.id.about)
 
-        val mActionBar=supportActionBar
+        val myWebView: WebView = findViewById(R.id.about)
+
+        val mActionBar = supportActionBar
         mActionBar!!.hide()
-        val assetManager=this.assets
-        try{
-        val inputStream=assetManager.open("about.html")
-        val size=inputStream.available()
-        val buffer=ByteArray(size)
+        val assetManager = this.assets
+
+        try {
+        val inputStream = assetManager.open("about.html")
+        val size = inputStream.available()
+        val buffer = ByteArray(size)
         inputStream.read(buffer)
         inputStream.close()
-        val content=String(buffer,StandardCharsets.UTF_8)
-        Log.d("DATA",content)
-        myWebView?.loadData(content,"text/html","utf-8")
-        }catch(e:IOException){
+        val content = String(buffer, StandardCharsets.UTF_8)
+        Log.d("DATA", content)
+        myWebView?.loadData(content, "text/html", "utf-8")
+        } catch (e: IOException) {
         e.printStackTrace()
         }
         }
