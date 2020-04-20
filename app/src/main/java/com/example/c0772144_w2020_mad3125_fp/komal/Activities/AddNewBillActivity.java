fbcb9a8b2f4e7e1cfdff7c161dd16c09f356b4fb
win10 <komalsubhra6768@gmail.com>
2020-04-20 15:14:52 -0400

    intilaizing fields

--- app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AddNewBillActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AddNewBillActivity.java
@@ -1,4 +1,38 @@
 package com.example.c0772144_w2020_mad3125_fp.komal.Activities;
 
-public class AddNewBillActivity {
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.c0772144_w2020_mad3125_fp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class AddNewBillActivity extends AppCompatActivity {
+    @BindView(R.id.spnBillType)
+    Spinner spnBillType;
+    @BindView(R.id.edtBillId)
+    TextInputLayout edtBillId;
+    @BindView(R.id.edtBillIdText)
+    TextInputEditText edtBillIdText;
+    @BindView(R.id.edtNumber) TextInputLayout edtNumber;
+    @BindView(R.id.edtNumberText) TextInputEditText edtNumberText;
+    @BindView(R.id.edtBillDate) TextInputLayout edtBillDate;
+    @BindView(R.id.edtBillDateText) TextInputEditText edtBillDateText;
+    @BindView(R.id.edtDataUsed) TextInputLayout edtDataUsed;
+    @BindView(R.id.edtDataUsedText) TextInputEditText edtDataUsedText;
+    @BindView(R.id.edtMinsUsed) TextInputLayout edtMinsUsed;
+    @BindView(R.id.edtMinsUsedText) TextInputEditText edtMinsUsedText;
+    @BindView(R.id.edtManufacName) TextInputLayout edtManufacName;
+    @BindView(R.id.edtManufacNameText) TextInputEditText edtManufacNameText;
+    @BindView(R.id.edtPlanName) TextInputLayout edtPlanName;
+    @BindView(R.id.edtPlanNameText) TextInputEditText edtPlanNameText;
+    @BindView(R.id.edtUnitsUsed) TextInputLayout edtUnitsUsed;
+    @BindView(R.id.edtUnitsUsedText) TextInputEditText edtUnitsUsedText;
+    @BindView(R.id.edtAgencyName) TextInputLayout edtAgencyName;
+    @BindView(R.id.edtAgencyNameText) TextInputEditText edtAgencyNameText;
+    @BindView(R.id.btnBillAdd)
+    Button btnBillAdd;
+
 }
