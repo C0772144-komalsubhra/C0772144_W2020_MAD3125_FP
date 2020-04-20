bd5d999783d5afaf7f5af4282305bd1164eba715
win10 <komalsubhra6768@gmail.com>
2020-04-20 15:12:15 -0400

    try catch in about activity

--- app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AboutWebActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AboutWebActivity.java
@@ -1,12 +1,25 @@
 package com.example.c0772144_w2020_mad3125_fp.komal.Activities;
 
 class AboutWebActivity  : AppCompatActivity()  {
-        override fun onCreate(savedInstanceState: Bundle?) {
+        override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
-    val myWebView: WebView = findViewById(R.id.about)
+        val myWebView:WebView=findViewById(R.id.about)
 
-    val mActionBar = supportActionBar
-    mActionBar!!.hide()
-    val assetManager = this.assets
+        val mActionBar=supportActionBar
+        mActionBar!!.hide()
+        val assetManager=this.assets
+        try{
+        val inputStream=assetManager.open("about.html")
+        val size=inputStream.available()
+        val buffer=ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        val content=String(buffer,StandardCharsets.UTF_8)
+        Log.d("DATA",content)
+        myWebView?.loadData(content,"text/html","utf-8")
+        }catch(e:IOException){
+        e.printStackTrace()
+        }
+        }
 }
