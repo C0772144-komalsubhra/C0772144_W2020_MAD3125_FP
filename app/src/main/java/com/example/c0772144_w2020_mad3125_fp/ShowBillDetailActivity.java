e6d614962f05a04f5c9ab55a904f114ef8fbc6a7
win10 <komalsubhra6768@gmail.com>
2020-04-20 14:59:37 -0400

    (Komal Subhra) initializing feilds

--- app/src/main/java/com/example/c0772144_w2020_mad3125_fp/ShowBillDetailActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_fp/ShowBillDetailActivity.java
@@ -1,4 +1,17 @@
 package com.example.c0772144_w2020_mad3125_fp;
 
-public class ShowBillDetailActivity {
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ShowBillDetailActivity extends AppCompatActivity {
+    private RecyclerView rvBillsList;
+    public static ArrayList<Bill> billsArrayListDetail = new ArrayList<>();
+    private BillsAdapter billsAdapter;
+    private ImageView imgAddButton;
+    private TextView txtTotalAmountValue;
 }
