63ba5566caa7adb49f2e93cc52d420ba90e149f0
win10 <komalsubhra6768@gmail.com>
2020-04-20 15:10:15 -0400

    updated about activity

--- app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AboutWebActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AboutWebActivity.java
@@ -1,4 +1,9 @@
 package com.example.c0772144_w2020_mad3125_fp.komal.Activities;
 
-class AboutWebActivity {
+class AboutWebActivity  : AppCompatActivity()  {
+    val myWebView: WebView = findViewById(R.id.about)
+
+    val mActionBar = supportActionBar
+    mActionBar!!.hide()
+    val assetManager = this.assets
 }
