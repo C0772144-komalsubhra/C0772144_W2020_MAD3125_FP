f1b57cd30d9457ad27290cf6a74770c10ef211f5
win10 <komalsubhra6768@gmail.com>
2020-04-20 15:11:21 -0400

    about activity changes

--- app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AboutWebActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_fp/komal/Activities/AboutWebActivity.java
@@ -1,6 +1,9 @@
 package com.example.c0772144_w2020_mad3125_fp.komal.Activities;
 
 class AboutWebActivity  : AppCompatActivity()  {
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
     val myWebView: WebView = findViewById(R.id.about)
 
     val mActionBar = supportActionBar
